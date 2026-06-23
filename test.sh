21cf596535cea92e691f9dbfc9d8c30713de15f3
Isac-Andrade <andrade.isac@icloud.com>
2026-06-22 22:02:45 -0300

    colors in test output

--- test.sh
+++ test.sh
@@ -2,34 +2,38 @@
 
 echo "=== Rodando Tests ==="
 
+VERDE='\033[0;32m'
+VERMELHO='\033[0;31m'
+SEM_COR='\033[0m'
+
 # Teste 1: disk-monitor
 RESULTADO=$(./Scripts/disk-monitor.sh)
 if [ -n "$RESULTADO" ]; then
-    echo "PASS: disk-monitor"
+    echo -e "${VERDE}PASS: disk-monitor${SEM_COR}"
 else
-    echo "FAIL: disk-monitor"
+    echo -e "${VERMELHO}FAIL: disk-monitor${SEM_COR}"
 fi
 
 # Teste 2: helthcheck
 RESULTADO=$(./Scripts/helthcheck.sh)
 if [ -n "$RESULTADO" ]; then
-    echo "PASS: helthcheck"
+    echo -e "${VERDE}PASS: helthcheck${SEM_COR}"
 else
-    echo "FAIL: helthcheck"
+    echo -e "${VERMELHO}FAIL: helthcheck${SEM_COR}"
 fi
 
 # Teste 3: log-clean
 RESULTADO=$(./Scripts/log-clean.sh)
 if [ -n "$RESULTADO" ]; then
-    echo "PASS: log-clean"
+    echo -e "${VERDE}PASS: log-clean${SEM_COR}"
 else
-    echo "FAIL: log-clean"
+    echo -e "${VERMELHO}FAIL: log-clean${SEM_COR}"
 fi
 
 # Teste 4: backup
 RESULTADO=$(./Scripts/backup.sh)
 if [ -n "$RESULTADO" ]; then
-    echo "PASS: backup"
+    echo  -e "${VERDE}PASS: backup${SEM_COR}"
 else
-    echo "FAIL: backup"
+    echo -e "${VERMELHO}FAIL: backup${SEM_COR}"
 fi
