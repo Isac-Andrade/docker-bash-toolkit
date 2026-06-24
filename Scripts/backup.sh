4ee60309407b8e636c539e6e08f30f6b4f8ad0f8
Isac-Andrade <andrade.isac@icloud.com>
2026-06-23 21:32:32 -0300

    execution log in the scripts

--- Scripts/backup.sh
+++ Scripts/backup.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
+DIR=$(dirname "$0")
+
 DATA=$(date +%Y-%m-%d)
 echo $DATA
 
 tar -czf backup-$DATA.tar.gz .
+
+echo "$(date '+%Y-%m-%d %H:%M:%S') - Backup criado: backup-$DATA.tar.gz" >> "$DIR/../logs/execucao.log"
 echo "Backup criado: backup-$DATA.tar.gz"
--- Scripts/log-clean.sh
+++ Scripts/log-clean.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+DIR=$(dirname "$0")
+
 DIAS=${1:-365}
 
 find . -name "*.log" -mtime +$DIAS -delete
 
+echo "$(date '+%Y-%m-%d %H:%M:%S') - Logs com mais de $DIAS dias foram removidos" >> "$DIR/../logs/execucao.log"
 echo "Logs com mais de $DIAS dias foram removidos"
